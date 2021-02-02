5282ed23c52461cec37ea90ec0ec7e43f6ba55bc
yunho <seo1511kr@gmail.com>
2021-02-02 23:47:28 +0900

    recursivehanoi

--- pycharm/codingtest/백준 재귀.py
+++ pycharm/codingtest/백준 재귀.py
@@ -1,4 +1,4 @@
-# 11729번
+# 11729번 하노이탑
 
 # 세 개의 장대가 있고 첫 번째 장대에는 반경이 서로 다른 n개의 원판이 쌓여 있다.
 # 각 원판은 반경이 큰 순서대로 쌓여있다. 이제 수도승들이 다음 규칙에 따라 첫 번째 장대에서
@@ -8,12 +8,17 @@
 # 쌓아 놓은 원판은 항상 위의 것이 아래의 것보다 작아야 한다.
 # 이 작업을 수행하는데 필요한 이동 순서를 출력하는 프로그램을 작성하라.
 # 단, 이동 횟수는 최소가 되어야 한다.
-#
-# 아래 그림은 원판이 5개인 경우의 예시이다.
 
-# 하노이탑
 
-# N=int(input())
-#
-# li=[i+1 for i in range(N)]
-#dp로 풀어야하나?
+
+N=int(input())
+def hanoi(N,start,assist,target):
+        if N==1:
+
+            return [start+" "+target]
+        return hanoi(N-1,start,target,assist)+[start+" "+target]+hanoi(N-1,assist,start,target)
+
+print(len(hanoi(N,"1","2","3")))
+print("\n".join(hanoi(N,"1","2","3")))
+
+
